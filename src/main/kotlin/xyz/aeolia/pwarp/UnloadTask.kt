de729358a52dddaa70689e536ab8f30327426b0e
Asheiou <31135263+Asheiou@users.noreply.github.com>
2025-08-18 14:04:24 +0100

    concurrentmodificationexception

--- src/main/kotlin/xyz/aeolia/pwarp/UnloadTask.kt
+++ src/main/kotlin/xyz/aeolia/pwarp/UnloadTask.kt
@@ -4,7 +4,7 @@ import org.bukkit.scheduler.BukkitRunnable
 
 class UnloadTask : BukkitRunnable() {
   override fun run() {
-    for (uuid in WarpPlayer.players.keys) {
+    for (uuid in WarpPlayer.players.keys.toList()) {
       WarpPlayer.unloadPlayer(uuid)
     }
   }
